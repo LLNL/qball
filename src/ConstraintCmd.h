d73e49505ae606530cabca7680be8c63fcf7399b
Erik Draeger <draeger1@llnl.gov>
2013-01-29 19:36:14 +0000

    Prepped headers for source release.

--- src/ConstraintCmd.h
+++ src/ConstraintCmd.h
@@ -1,17 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////
 //
-// Copyright (c) 2008 The Regents of the University of California
-//
-// This file is part of Qbox
-//
-// Qbox is distributed under the terms of the GNU General Public License
-// as published by the Free Software Foundation, either version 2 of
-// the License, or (at your option) any later version.
-// See the file COPYING in the root directory of this distribution
-// or <http://www.gnu.org/licenses/>.
-//
-////////////////////////////////////////////////////////////////////////////////
-//
 // ConstraintCmd.h:
 //
 ////////////////////////////////////////////////////////////////////////////////
