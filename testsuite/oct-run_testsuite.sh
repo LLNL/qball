6e1eec824d5f28a0c8aa765a4ef299b32900623d
Xavier Andrade <xavier@tddft.org>
2016-03-24 21:11:04 +0000

    Replaced 'octopus' by 'qb@ll' in output.

--- testsuite/oct-run_testsuite.sh
+++ testsuite/oct-run_testsuite.sh
@@ -240,7 +240,7 @@ done
 shift $[ OPTIND - 1 ]
 
 echo "*****************************"
-echo "  Running octopus testsuite  "
+echo "  Running qb@ll testsuite  "
 echo "*****************************"
 echo
 echo " Note: Running the testsuite can take from several minutes"
@@ -260,7 +260,7 @@ fi
 if [ -z $OCT_TEST_NJOBS ] ; then
 
     echo
-    echo " The environment variable OCT_TEST_NJOBS is not set. Octopus"
+    echo " The environment variable OCT_TEST_NJOBS is not set. qb@ll"
     echo " will determine automatically an appropriate number of jobs:"
     echo
     
@@ -293,7 +293,7 @@ if [ -z $OCT_TEST_NJOBS ] ; then
 	fi
     fi
     
-    options=" "`$bin_directory/octopus -c`" "
+    options=" "`$bin_directory/qb-$ARCH -c`" "
     if [[ $options =~ " mpi " ]]; then
 	OCT_TEST_NJOBS=$(($OCT_TEST_NJOBS/4))
 	echo "  - This is an MPI run. Each job can use up to 4 processors."
