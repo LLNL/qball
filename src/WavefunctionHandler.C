63f5ccee6db748d4c66899827353c8327f99be66
Xavier Andrade <xavier@tddft.org>
2016-07-06 08:19:32 -0700

    Bugfix: use if instead of ifdef for macro check.

--- src/WavefunctionHandler.C
+++ src/WavefunctionHandler.C
@@ -430,7 +430,7 @@ void WavefunctionHandler::endElement(const XMLCh* const uri,
         // base64 encoding
         unsigned int length;
 
-#ifdef XERCES_VERSION_MAJOR >= 3
+#if XERCES_VERSION_MAJOR >= 3
         XMLByte* b = Base64::decode((XMLByte*)content.c_str(),
                                     (XMLSize_t*) &length);
 #else
