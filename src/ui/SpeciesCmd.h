4e95a5be681e5d5c2b10cc6d192b0393c9944a86
Xavier Iago Andrade-valencia <andradevalen1@llnl.gov>
2018-05-15 15:21:41 -0700

    Moved the code of SpeciesCmd to the header.

--- src/ui/SpeciesCmd.h
+++ src/ui/SpeciesCmd.h
@@ -38,6 +38,8 @@ using namespace std;
 
 #include <ui/UserInterface.h>
 #include <qball/Sample.h>
+#include <qball/SpeciesReader.h>
+#include <qball/Species.h>
 
 class SpeciesCmd : public Cmd
 {
@@ -57,7 +59,51 @@ class SpeciesCmd : public Cmd
     "   The species command defines a species name.\n\n";
   }
 
-  int action(int argc, char **argv);
+  int action(int argc, char **argv) {
+    if (! (argc == 3 || argc == 4)) {
+      if ( ui->oncoutpe() )
+	cout << "  <!-- use: species name uri [ewald_width] -->" << endl;
+      return 1;
+    }
+  
+    if ( ui->oncoutpe() )
+      cout << "  <!-- SpeciesCmd: defining species " << argv[1]
+	   << " as " << argv[2] << " -->" << endl;
+
+    SpeciesReader sp_reader(s->ctxt_);
+  
+    Species* sp = new Species(s->ctxt_,argv[1]);
+  
+    try {
+      sp_reader.readSpecies(*sp,argv[2]);
+      sp_reader.bcastSpecies(*sp);
+      if (argc == 4) {
+	const double rcpsin = atof(argv[3]);
+	s->atoms.addSpecies(sp,argv[1],rcpsin);
+      }
+      else
+	s->atoms.addSpecies(sp,argv[1]);
+
+      if (sp->ultrasoft()) {
+	s->ctrl.ultrasoft = true;
+	s->wf.set_ultrasoft(true);
+      }
+    
+      if (sp->nlcc()) {
+	s->ctrl.nlcc = true;
+      }
+    }
+    catch ( const SpeciesReaderException& e ) {
+      cout << " SpeciesReaderException caught in SpeciesCmd" << endl;
+      cout << " SpeciesReaderException: cannot define Species" << endl;
+    }
+    catch (...) {
+      cout << " SpeciesCmd: cannot define Species" << endl;
+    }
+  
+    return 0;
+  }
+
 };
 #endif
 
