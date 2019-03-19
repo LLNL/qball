56554269662ed8cd6ef8f70acccba3aacf0b5a84
Xavier Andrade <xavier@tddft.org>
2019-03-19 11:37:17 -0700

    Moved the calculation of (k + g + a)^2 to the vector potential module.

--- src/qball/VectorPotential.h
+++ src/qball/VectorPotential.h
@@ -28,6 +28,7 @@
 
 #include <config.h>
 #include <math/d3vector.h>
+#include <qball/Basis.h>
 
 #ifndef VECTORPOTENTIAL_H
 #define VECTORPOTENTIAL_H
@@ -41,6 +42,17 @@ public:
     value_(initial_value),
     value2_(norm(value_))
   {
+  }
+
+  double * get_kpgpa2(const Basis & basis) const {
+    double * kpgpa2 = new double[basis.localsize()];
+    for(int ig = 0; ig < basis.localsize(); ig++){
+      kpgpa2[ig] = basis.kpg2_ptr()[ig] + value2();
+      kpgpa2[ig] -= value_[0]*basis.kpgx_ptr(0)[ig];
+      kpgpa2[ig] -= value_[1]*basis.kpgx_ptr(1)[ig];
+      kpgpa2[ig] -= value_[2]*basis.kpgx_ptr(2)[ig];
+    }
+    return kpgpa2;
   }
 
   const D3vector & value() const {
